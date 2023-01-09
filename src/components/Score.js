f2f7f5aff0fdfe963b46246bd80ec44d16b203fa
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-01-09 00:34:55 -0600

    moving score reset to Level.js

--- src/components/Score.js
+++ src/components/Score.js
@@ -6,33 +6,32 @@
 /* END-USER-IMPORTS */
 
 class Score extends UserComponent {
+  constructor(gameObject) {
+    super(gameObject);
 
-	constructor(gameObject) {
-		super(gameObject);
+    this.gameObject = gameObject;
+    gameObject["__Score"] = this;
 
-		this.gameObject = gameObject;
-		gameObject["__Score"] = this;
-
-		/* START-USER-CTR-CODE */
+    /* START-USER-CTR-CODE */
     // Write your code here.
     /* END-USER-CTR-CODE */
 
-		// custom definition props
-		this.score = 0;
-		this.maxScore = 3;
-		this.minScore = 0;
-		this.player;
-	}
+    // custom definition props
+    this.score = 0;
+    this.maxScore = 3;
+    this.minScore = 0;
+    this.player;
+  }
 
-	/** @returns {Score} */
-	static getComponent(gameObject) {
-		return gameObject["__Score"];
-	}
+  /** @returns {Score} */
+  static getComponent(gameObject) {
+    return gameObject["__Score"];
+  }
 
-	/** @type {Phaser.GameObjects.Image} */
-	gameObject;
+  /** @type {Phaser.GameObjects.Image} */
+  gameObject;
 
-	/* START-USER-CODE */
+  /* START-USER-CODE */
 
   gainPoint() {
     this.score += 1;
@@ -47,7 +46,6 @@ class Score extends UserComponent {
     if (this.score === this.maxScore) {
       // emit winner event
       this.gameObject.scene.events.emit(GAME_OVER, this.player);
-      this.score = 0;
     }
   }
 
